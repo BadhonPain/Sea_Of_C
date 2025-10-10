5e8a4aa95c38c22944a645f53417529d7492c1cf
Test_Case <badhonpain48@gmail.com>
2025-10-10 07:45:49 +0600

    little change in array and struct ; new content loop

--- Array/sorting_ascending.c
+++ Array/sorting_ascending.c
@@ -7,8 +7,8 @@ int main()
     int a[n];
     for (int i = 0; i < n; i++)
         scanf("%d", &a[i]);
-    for (int i = 0; i < n - 1; i--)
-        for (int j = i + 1; j < n; j--)
+    for (int i = 0; i < n - 1; i++)
+        for (int j = i + 1; j < n; j++)
             if (a[i] > a[j])
             {
                 int t = a[i];
